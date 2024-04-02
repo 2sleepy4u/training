3c45f85c0906a7fe453ac184963425ae77d3b9f6
2sleepy4u <riccardo.zancan00@gmail.com>
2024-04-02 17:53:02 +0200

    Cleaned main.rs and added a gitignore

--- rustyserver/src/main.rs
+++ rustyserver/src/main.rs
@@ -8,20 +8,34 @@ mod auth;
 use auth::routes::*;
 
 
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-
 #[options("/<_..>")]
 fn request_roll_preflight() {}
 
 #[catch(401)]
 fn unauthorized(req: &Request) -> String {
-    req.cookies().remove(Cookie::named("SSID"));
+    req.cookies().remove(Cookie::from("SSID"));
     "Ops Not Authorized".to_owned()
+}
+
+#[launch]
+fn rocket() -> _ {
+    println!("Starting server...");
+
+    rocket::build()
+        .attach(AdHoc::on_response("Add CORS", |_req, response| Box::pin(async move {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+        })))
+        .attach(Training::init())
+        .mount("/", routes![
+               request_roll_preflight,
+               get_new_session
+        ])
+        .register("/", catchers![unauthorized])
+            
 }
 
 /*
@@ -39,24 +53,3 @@ async fn get_new_session(mut db: Connection<M2M>) -> Option<String> {
         .ok()
 }
 */
-#[launch]
-fn rocket() -> _ {
-    println!("Starting server...");
-
-    rocket::build()
-        .attach(AdHoc::on_response("Add CORS", |_req, response| Box::pin(async move {
-            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-            response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-
-        })))
-        .attach(Training::init())
-        .mount("/", routes![
-               index, 
-               request_roll_preflight,
-               get_new_session
-        ])
-        .register("/", catchers![unauthorized])
-            
-}
